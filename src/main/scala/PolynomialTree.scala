477f66db7da905740dcdc6bca915856f0aa43788
Torstein Vik <torsteinv64@gmail.com>
2017-12-08 23:07:05 +0100

    Reordered polynomial nodes by precedence

--- src/main/scala/PolynomialTree.scala
+++ src/main/scala/PolynomialTree.scala
@@ -2,15 +2,15 @@ package io.github.torsteinvik.oeisgloballyrecursivemult
 
 abstract sealed class PolynomialTree {
     import PolynomialTree._
-    def :* (right : PolynomialTree) = new :*(this, right)
     def :** (exp : Int) = new :**(this, exp)
+    def :* (right : PolynomialTree) = new :*(this, right)
     def :+ (right : PolynomialTree) = new :+(this, right)
     def :- (right : PolynomialTree) = new :-(this, right)
 }
 
 object PolynomialTree {
-    case class :*  (left : PolynomialTree, right : PolynomialTree) extends PolynomialTree
     case class :** (base : PolynomialTree, exp : Int)              extends PolynomialTree
+    case class :*  (left : PolynomialTree, right : PolynomialTree) extends PolynomialTree
     case class :+  (left : PolynomialTree, right : PolynomialTree) extends PolynomialTree
     case class :-  (left : PolynomialTree, right : PolynomialTree) extends PolynomialTree
 
