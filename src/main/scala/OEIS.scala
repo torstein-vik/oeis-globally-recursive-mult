827132123231adc369ded1fc85bcf71f3ae9fb9b
Torstein Vik <torsteinv64@gmail.com>
2017-12-09 00:47:08 +0100

    Added Int => OEIS def

--- src/main/scala/OEIS.scala
+++ src/main/scala/OEIS.scala
@@ -1,3 +1,7 @@
 package io.github.torsteinvik.oeisgloballyrecursivemult
 
 case class OEIS (id : String)
+
+object OEIS {
+    def fromNumber (n : Int) = if (0 < n && n <= 999999) OEIS("A%06d".format(n)) else throw new Exception("Not a valid OEIS ID number!")
+}
