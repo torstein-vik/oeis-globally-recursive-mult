38c5bce1cc8d14e63a8ca42c3e23109006c64b2a
Torstein Vik <torsteinv64@gmail.com>
2017-12-09 00:30:00 +0100

    Made parser parse a sequence of formulas instead of just one

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -8,7 +8,7 @@ object Parser extends RegexParsers {
     
     def parseFile (src : Source) = getFormulae(new CharSequenceReader(src.mkString))
     
-    def getFormulae (in : Input) : Seq[(OEIS, Recursion)] = parseAll(formulae, in) match {
+    def getFormulae (in : Input) : Seq[(OEIS, Seq[PolynomialTree])] = parseAll(formulae, in) match {
         case Success(x, _) => x
         case Failure(msg, next) => throw new Exception("Error at line " + next.pos.line + " column " + next.pos.column + "\n" + next.pos.longString + "\n" + msg)
         case Error(msg, next) => throw new Exception("Error at line " + next.pos.line + " column " + next.pos.column + "\n" + next.pos.longString + "\n" + msg)
