31494a4131fe1836b2d376b15d9b3d0842ffbf28
Torstein Vik <torsteinv64@gmail.com>
2017-12-08 21:05:11 +0100

    Added parser boilerplate

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -7,5 +7,12 @@ import scala.io.Source
 object Parser extends RegexParsers {
     
     
+    def getFormulae (in : Input) : Seq[(OEIS, Recursion)] = parseAll(formulae, in) match {
+        case Success(x, _) => x
+        case Failure(msg, next) => throw new Exception("Error at line " + next.pos.line + " column " + next.pos.column + "\n" + next.pos.longString + "\n" + msg)
+        case Error(msg, next) => throw new Exception("Error at line " + next.pos.line + " column " + next.pos.column + "\n" + next.pos.longString + "\n" + msg)
+    }
+
+    def formulae : Parser[Seq[(OEIS, Recursion)]] = ???
     
 }
