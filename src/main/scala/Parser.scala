a6adb58e92c5c2fe96155a1c907618b8fa5525c5
Torstein Vik <torsteinv64@gmail.com>
2017-12-09 00:35:01 +0100

    Added parser with polynomial with 0 operator precedence

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -24,6 +24,7 @@ object Parser extends RegexParsers {
     def formulaswrapped : Parser[Seq[PolynomialTree]] = "x = SR.var('x')" ~> "return { 'ogf': " ~> formulas <~ "}"
     def formulas : Parser[Seq[PolynomialTree]] = "[" ~> repsep(polynomial0, ",") <~ "]"
     
+    def polynomial0 : Parser[PolynomialTree] = terms | factors | div | pow | variable | number | paren
     
     import PolynomialTree._
     
