f3a44eb0240f79ee909a4d39d26b7614cfa3b18c
Torstein Vik <torsteinv64@gmail.com>
2017-12-08 21:07:43 +0100

    Added polynomial tree operators

--- src/main/scala/PolynomialTree.scala
+++ src/main/scala/PolynomialTree.scala
@@ -3,4 +3,9 @@ package io.github.torsteinvik.oeisgloballyrecursivemult
 abstract sealed class PolynomialTree
 
 object PolynomialTree {
+    case class :*:  (left : PolynomialTree, right : PolynomialTree) extends PolynomialTree
+    case class :**: (base : PolynomialTree, exp : Int)              extends PolynomialTree
+    case class :+:  (left : PolynomialTree, right : PolynomialTree) extends PolynomialTree
+    case class :-:  (left : PolynomialTree, right : PolynomialTree) extends PolynomialTree
+
 }
