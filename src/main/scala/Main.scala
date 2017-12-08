f4448959766d30884987b00c8beb2e1d19cd0465
Torstein Vik <torsteinv64@gmail.com>
2017-12-08 21:02:58 +0100

    Added oeis gf source to main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,10 @@
 package io.github.torsteinvik.oeisgloballyrecursivemult
 
+import scala.io.Source
+
 object Main extends App {
     
+    val source = Source.fromURL(getClass.getResource("/oeis_gf.py"))
     
     
 }
