adf0e6591d6e893d995a3e90107a2c8f8d1b11cf
Torstein Vik <torsteinv64@gmail.com>
2017-12-09 04:39:38 +0100

    Added internal hashset to multiplicative object

--- src/main/scala/Multiplicative.scala
+++ src/main/scala/Multiplicative.scala
@@ -3,4 +3,9 @@ package io.github.torsteinvik.oeisgloballyrecursivemult
 import scala.io.Source
 
 object Multiplicatives {
+    import collection.mutable.HashSet
+    
+    private var multiplicatives : HashSet[OEIS] = new HashSet()
+    
+    
 }
